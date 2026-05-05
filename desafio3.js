8878c7a331f7961f77d7a94e0bac74ecc06e09b1
HHenrilM <hendril.henrique@hotmail.com>
2026-05-05 19:43:58 -0300

    Escrevendo as classes de um Jogo

---
+++ desafio3.js
@@ -0,0 +1,21 @@
+/*
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+*/
+const readline = require('readline'); 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log('Um herói de uma aventura');
+
